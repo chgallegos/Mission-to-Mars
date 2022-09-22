4a70d2a4a08388847bac6b727edb6d2b2a1799e3
ChrisG <chrisale89@gmail.com>
2022-09-22 14:45:51 -0600

    corrected line 21 of py.app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from contextlib import redirect_stderr
+from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scraping
 
@@ -18,7 +19,7 @@ def scrape():
    mars = mongo.db.mars
    mars_data = scraping.scrape_all()
    mars.update_one({}, {"$set":mars_data}, upsert=True)
-   return "Scraping Complete" 
+   return redirect('/', code=302)
 
 if __name__ == "__main__":
     app.run(debug=True)
